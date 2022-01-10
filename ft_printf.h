3d08f5719a6463240325a6cac9f9d7ef90934f80
Fteeliy <Fteeliy@yandex.ru>
2022-01-10 16:00:14 +0300

    added files

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: wdwain <wdwain@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 14:28:31 by wdwain            #+#    #+#             */
-/*   Updated: 2022/01/10 15:30:59 by wdwain           ###   ########.fr       */
+/*   Updated: 2022/01/10 15:59:37 by wdwain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,18 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-int	ft_printf(const char *format, ...);
-int	parse_input(const char *format, va_list ap, int length);
+int	ft_putchar(char c, int *length);
+void	ft_putnbr(int nbr, int *length);
+void	ft_putuns(unsigned int nbr, int *length);
+void	ft_puthex(char *str, unsigned long long nbr, int *length);
+void	c_type(va_list ap, int *length);
+void	s_type(va_list ap, int *length);
+void	d_i_type(va_list ap, int *length);
+void	u_type(va_list ap, int *length);
+void	x_type(char *str, va_list ap, int *length);
+void	p_type(char *str, va_list ap, int *length);
+void	parse_process(char *str, va_list ap, int *length);
+int		parse_input(const char *format, va_list ap, int length);
+int		ft_printf(const char *format, ...);
 
 #endif
